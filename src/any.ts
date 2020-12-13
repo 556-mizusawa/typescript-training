63efaf9adb3b0fbcf901695c5b61a6887708ea4c
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-13 20:21:29 +0900

    type any

--- src/any.ts
+++ src/any.ts
@@ -6,5 +6,12 @@ let url: string =
   'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
 
 axios.get(url).then(function (response) {
-  console.log(response);
+  interface Article {
+    id: number;
+    title: string;
+    description: string;
+  }
+  let data: Article[];
+  data = response.data;
+  console.log(data);
 });
