38de92535ba505ac87b3b8ecee3b727f9599e4ca
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-13 19:08:20 +0900

    Primitive type test

---
+++ src/boolean.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var name = 'TypeScript';
+var isFinished = true;
+isFinished = false;
+// isFinished = 1;
+console.log({ isFinished: isFinished });
---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+let year: number = 1988;
+
+console.log(year);
+
+let age: number = 0x20;
+
+console.log(age);
+
+
+let name: string = 'DLVI';
+console.log(name);
