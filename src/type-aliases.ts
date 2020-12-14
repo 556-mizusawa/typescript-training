1a0ff9d27e119d153b382f2f8ad00b5001eb00c0
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-14 17:15:41 +0900

    interface

--- src/type-aliases.ts
+++ src/type-aliases.ts
@@ -13,3 +13,15 @@ const example1: {
   name: 'DLVI',
   age: 32,
 };
+
+type Profile = {
+  name: string;
+  age: number;
+};
+
+const example2: Profile2 = {
+  name: 'DLVI',
+  age: 32,
+};
+
+type Profile2 = typeof example1;
