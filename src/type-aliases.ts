61e97b012107bba88d6ceb494f40ad85e0535b1d
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-14 17:01:21 +0900

    type aliases

---
+++ src/type-aliases.ts
@@ -0,0 +1,15 @@
+export {};
+
+type Mojiretsu = string;
+
+const fooString: string = 'Hello';
+
+const fooMojiretsu: Mojiretsu = 'Hello';
+
+const example1: {
+  name: Mojiretsu;
+  age: number;
+} = {
+  name: 'DLVI',
+  age: 32,
+};
