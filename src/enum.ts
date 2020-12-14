957931a1377209221a1a34dfc564421b7712158e
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-14 19:32:26 +0900

    type enum

---
+++ src/enum.ts
@@ -0,0 +1,40 @@
+export {};
+
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+Months.January;
+console.log(Months.April);
+console.log(Months.December);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  // YELLOW = '#FFFF00',
+  BLACK = '#000000',
+}
+
+let green = COLORS.GREEN;
+console.log({ green });
+console.log(COLORS.GREEN);
+
+enum COLORS {
+  YELLOW = '#FFFF00',
+  GRAY = '#808080',
+}
+
+COLORS.YELLOW;
