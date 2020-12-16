b4161f54be894814fc076ce5088b526e938cfa81
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-17 08:59:51 +0900

    const assertion

---
+++ src/as-const.ts
@@ -0,0 +1,15 @@
+export {};
+
+let name = 'kougoro';
+
+name = 'Mizusawa';
+
+let nickname = 'Oceans556' as const;
+// nickname = 'OceansDLVI';
+
+let profile = {
+  name: 'kougoro',
+  height: 165,
+};
+
+profile.name = 'Ocean';
